cb3c131c366cbec59485aadfd198fb7ccc5c2bab
haasche <haasche@gmail.com>
2020-09-23 09:45:10 -0500

    pushing to end basic tutorial

--- src/com/ehaasch/Main.java
+++ src/com/ehaasch/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Editing this file to practice fetch/ checking out / pulling");
+        System.out.println("Successfully retrieved, now pushing final update!");
     }
 }
